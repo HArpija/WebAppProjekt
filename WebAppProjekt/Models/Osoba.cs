931041d6980a88361ca6b9fba3ee960dc16e21cc
HArpija <Helga@DESKTOP-DAD59KM>
2023-03-07 11:26:15 +0100

    o

--- WebAppProjekt/Models/Osoba.cs
+++ WebAppProjekt/Models/Osoba.cs
@@ -3,12 +3,12 @@
     public class Osoba
     {
         public int Id { get; set; }
-        public string? Ime { get; set; }
-        public string? Prezime { get; set; }
-        public string? BrojTelefona { get; set; }
-        public string? BrojMobitela { get; set; }
-        public string? Adresa { get; set; }
-        public string? Grad { get; set; }
-        public string? Drzava { get; set; }
+        public string Ime { get; set; }
+        public string Prezime { get; set; }
+        public string BrojTelefona { get; set; }
+        public string BrojMobitela { get; set; }
+        public string Adresa { get; set; }
+        public string Grad { get; set; }
+        public string Drzava { get; set; }
     }
 }
